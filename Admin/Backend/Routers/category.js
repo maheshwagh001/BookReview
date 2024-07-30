9fe4492f6c9d692e2f01ecfbb5bd1eecc91027da
Mahesh <119623424+maheshwagh001@users.noreply.github.com>
2024-07-30 12:55:05 +0530

    Delete Admin/Backend directory

--- Admin/Backend/Routers/category.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const { addCategory, getCategory } = require("../Controllers/category")
-const router = express.Router()
-
-router.post("/addCategory", addCategory)
-router.get("/getCategory", getCategory)
-
-module.exports = router
